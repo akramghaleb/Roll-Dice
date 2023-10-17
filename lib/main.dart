ae3b8e357cd3befa43a9df301aa7cc764948bb86
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-10-17 13:58:09 +0300

    color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black45,
         body: Center(
           child: Text('Hello World'),
         ),
