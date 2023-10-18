76592b801e59b9cd0bdaaa5fd08aabdcb50d2a1e
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-10-18 11:53:00 +0300

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black45,
-        body: Center(
-          child: Text('Hello World'),
+        //backgroundColor: Colors.black45,
+        body: Container(
+          //color: Colors.amber,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromRGBO(218, 306, 237, 1),
+                Color.fromRGBO(90, 170, 245, 1)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
         ),
       ),
     ),
