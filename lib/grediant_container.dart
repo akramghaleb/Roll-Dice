c748126fb118579a2e2b9efb01d67154ef147812
aghammaricme <46090581+akramghaleb@users.noreply.github.com>
2023-10-21 11:40:56 +0300

    Update grediant_container.dart

--- lib/grediant_container.dart
+++ lib/grediant_container.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/my_custom_text_style.dart';
 
+const beginAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+// final getDate = getDate(); --> value change in run time; and can not in code
+// const beginAlignment = Alignment.topLeft; --> value can not be change in run time and in code
 class GrediantContainer extends StatelessWidget {
   const GrediantContainer({super.key});
 
@@ -10,8 +15,8 @@ class GrediantContainer extends StatelessWidget {
       //color: Colors.amber,
       decoration: const BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: beginAlignment,
+          end: endAlignment,
           colors: [
             Color.fromRGBO(218, 306, 237, 1),
             Color.fromRGBO(90, 170, 245, 1)
